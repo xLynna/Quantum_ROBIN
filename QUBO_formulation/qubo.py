05d05a10d5b016b0908f86097582fe7bab277663
xLynna <lynna.xie@gmail.com>
2023-06-05 20:29:43 +0100

    Implemented methods to extract k core

--- QUBO_formulation/qubo.py
+++ QUBO_formulation/qubo.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Without variable number limits
 def naive_definite_transformation(G, n):
     """Transform a graph G into a QUBO matrix Q and bias b for 
        the maximum clique problem.
@@ -27,6 +28,34 @@ def naive_definite_transformation(G, n):
 
     penalty_reg = 2
     
-    return penalty_reg*Q, b
+    return penalty_reg * Q, b
 
+# With variable number limits
+def extract_k_core(G, k):
+    """Extract the k-core of a graph G.
+    
+    Parameters
+    ----------
+    G : np.ndarray
+        Adjacency matrix of the graph.
+    k : int
+        Number of nodes in the k-core.
+        
+    Returns
+    -------
+    G : np.ndarray
+        Sparse adjacency matrix of the k-core.
+    """
+    G = G.copy()
+    while True:
+        degrees = np.sum(G, axis=0)
+        reduce_mask = degrees < k
+        if np.all(degrees[reduce_mask] == 0):
+            break
+        else:
+            G[reduce_mask, :] = 0
+            G[:, reduce_mask] = 0
+    return G
+
+    
     
