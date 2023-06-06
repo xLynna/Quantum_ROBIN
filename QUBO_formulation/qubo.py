ee0dec54def2eccdb4976167e10a011805751c6a
xLynna <lynna.xie@gmail.com>
2023-06-06 03:29:18 +0100

    added split function for cutting larger size MC graph

--- QUBO_formulation/qubo.py
+++ QUBO_formulation/qubo.py
@@ -1,7 +1,8 @@
 import numpy as np
+import heapq
 
 # Without variable number limits
-def naive_definite_transformation(G, n):
+def naive_definite_transformation(G):
     """Transform a graph G into a QUBO matrix Q and bias b for 
        the maximum clique problem.
     
@@ -9,8 +10,6 @@ def naive_definite_transformation(G, n):
     ----------
     G : np.ndarray
         Adjacency matrix of the graph.
-    n : int
-        Number of nodes in the graph (number of measurements).
         
     Returns
     -------
@@ -19,7 +18,7 @@ def naive_definite_transformation(G, n):
     b : numpy.ndarray
         Bias vector.
     """
-
+    n = G.shape[0]
     Q = np.zeros((n, n))
     b = -np.ones(n)
 
@@ -32,7 +31,7 @@ def naive_definite_transformation(G, n):
 
 # With variable number limits
 def extract_k_core(G, k):
-    """Extract the k-core of a graph G.
+    """Extract the k-core of a graph G inplace.
     
     Parameters
     ----------
@@ -43,19 +42,22 @@ def extract_k_core(G, k):
         
     Returns
     -------
-    G : np.ndarray
+    G : np.ndarray (inplace)
         Sparse adjacency matrix of the k-core.
+    ineices : np.ndarray
+        Indices of the remain nodes in the k-core.
     """
-    G = G.copy()
+    indices = np.arange(G.shape[0])
+
     while True:
-        degrees = np.sum(G, axis=0)
-        reduce_mask = degrees < k
-        if np.all(degrees[reduce_mask] == 0):
-            break
-        else:
-            G[reduce_mask, :] = 0
-            G[:, reduce_mask] = 0
-    return G
+      degrees = np.sum(G, axis=0)
+      remain_mask = degrees >= k
+      if np.all(degrees[remain_mask] >= k):
+          break
+      else:
+          indices = indices[remain_mask]
+          G = G[remain_mask, :][:, remain_mask]
+    return indices
 
 def reduce_graph(G, lower_bound):
     """Reduce the graph G to a subgraph with a minimum number of nodes.
@@ -69,17 +71,138 @@ def reduce_graph(G, lower_bound):
         
     Returns
     -------
-    G : np.ndarray
+    G : np.ndarray (inplace)
         Adjacency matrix of the subgraph.
+    indices : np.ndarray
+        Indices in terms of the input graph of the remain nodes in the subgraph.
     """
-    G = extract_k_core(G, lower_bound)
-    vertex_indices = np.argwhere(np.sum(G, axis=0) > 0).flatten()
-    v = np.random.choice(vertex_indices)
+    meta_indices = extract_k_core(G, lower_bound)
+    v = np.random.choice(len(meta_indices), 1)[0]
     v_neighbours_mask = G[v, :] == 1
     common_neighbours = np.logical_and(G, v_neighbours_mask).sum(axis=1, keepdims=True)
     # Remove the edges between the nodes if their common neighbours 
     # are less than the lower bound - 2
-    G[common_neighbours < (lower_bound - 2)] = 0
-    G = extract_k_core(G, lower_bound)
+    G[v, common_neighbours < (lower_bound - 2)] = 0
+    G[common_neighbours < (lower_bound - 2), v] = 0
+    sub_indices = extract_k_core(G, lower_bound)
+    return meta_indices[sub_indices]
+
+def highest_degree_node(G):
+    """Find the node with the highest degree in the graph G.
+    
+    Parameters
+    ----------
+    G : np.ndarray
+        Adjacency matrix of the graph.
+        
+    Returns
+    -------
+    v : int
+        Index of the node with the highest degree.
+    """
+    degrees = np.sum(G, axis=0)
+    v = np.argmax(degrees)
+    return v
+
+def extract_subgraph(G, v):
+    """Extract the subgraph of G induced by the neighbours of v.
+    
+    Parameters
+    ----------
+    G : np.ndarray
+        Adjacency matrix of the graph.
+    v : int
+        Index of the node.
+        
+    Returns
+    -------
+    G : np.ndarray
+        Adjacency matrix of the subgraph.
+    """
+    indices = np.arange(G.shape[0])
+    v_neighbours_mask = G[v, :] == 1
+    G = G[v_neighbours_mask, :][:, v_neighbours_mask].copy()
+    return G, indices[v_neighbours_mask]
+
+def remove_vertex(G, v):
+    """Remove the node v from the graph G.
+    
+    Parameters
+    ----------
+    G : np.ndarray
+        Adjacency matrix of the graph.
+    v : int
+        Index of the node.
+        
+    Returns
+    -------
+    G : np.ndarray
+        Adjacency matrix of the subgraph.
+    """
+    G = np.delete(G, v, axis=0)
+    G = np.delete(G, v, axis=1)
     return G
+
+def solve(G):
+   pass
+   #return max_clique_mask
+
+def split(G, vertex_limit, lower_bound):
+    """Split the graph G into subgraphs with a maximum number of nodes.
+    
+    Parameters
+    ----------
+    G : np.ndarray
+        Adjacency matrix of the graph.
+    vertex_limit : int
+        Maximum number of nodes in the subgraphs.
+    lower_bound : int
+        Minimum number of nodes in the subgraphs.
+        
+    Returns
+    -------
+    subGs : list
+        List of adjacency matrices of the subgraphs.
+    """
+    max_clique_indices = None
+    # negate graph size, graph, remaining nodes, splitted nodes
+    subgraphs = [(-G.shape[0], G, np.arange[G.shape[0]] , set())]
+
+    while len(subgraphs) > 0 and -subgraphs[0][0] > vertex_limit:
+      _, sg, sg_indices, sg_pending_indices = heapq.heappop(subgraphs)
+      v_ind = highest_degree_node(sg)
+      ssg, ssg_raw_indeices = extract_subgraph(sg, v_ind)
+      # indices in terms of the input graph
+      ssg_indices = sg_indices[ssg_raw_indeices]
+
+      sg = remove_vertex(sg, v_ind)
+      sg_indices = np.delete(sg_indices, v_ind)
+
+      # Calculate the max clique from sg
+      sg_indices = sg_indices[reduce_graph(sg, lower_bound)]
+      if sg_indices.shape[0] > 0:
+        if sg_indices.shape[0] <= vertex_limit:
+          # Obtain the max clique size and the related binary solution of the subgraph
+          sg_solution_mask = solve(sg)
+          # The size should include the vertices removed due to vertex limit
+          combined_clique_size = len(sg_solution_mask) + len(sg_pending_indices)
+          if combined_clique_size > lower_bound:
+            lower_bound = combined_clique_size
+            max_clique_indices = set(sg_indices[sg_solution_mask]) + sg_pending_indices
+        else:
+          heapq.heappush(subgraphs, (-sg.shape[0], sg, sg_indices, sg_pending_indices))
+      
+      # Calculate the max clique from ssg
+      ssg_indices = ssg_indices[reduce_graph(ssg, lower_bound)]
+      if ssg_indices.shape[0] > 0:
+        if ssg_indices.shape[0] <= vertex_limit:
+          ssg_solution_mask = solve(ssg)
+          combined_clique_size = len(ssg_solution_mask) + len(sg_pending_indices) + 1 # v_ind
+          if combined_clique_size > lower_bound:
+            lower_bound = combined_clique_size
+            max_clique_indices = set(ssg_indices[ssg_solution_mask]) + sg_pending_indices + {v_ind}
+        else:
+          heapq.heappush(subgraphs, (-ssg.shape[0], ssg, ssg_indices, sg_pending_indices + {v_ind}))
+
+    return max_clique_indices, lower_bound
     
