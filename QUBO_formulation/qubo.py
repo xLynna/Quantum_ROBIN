6001fe0d11d67470f21982f01802ce6dd743e1ac
xLynna <lynna.xie@gmail.com>
2023-06-08 00:18:08 +0100

    Added functions to compute weight graph to qubo

--- QUBO_formulation/qubo.py
+++ QUBO_formulation/qubo.py
@@ -3,7 +3,7 @@ import heapq
 from Solvers.solver import solve_qubo
 
 # Without variable number limits
-def definite_graph_to_qubo(G):
+def definite_graph_to_qubo(G, penalty_reg=2):
   """Transform a graph G into a QUBO matrix Q and bias b for 
       the maximum clique problem.
   
@@ -25,9 +25,29 @@ def definite_graph_to_qubo(G):
 
   Q[G == 0] = 1 # assign unit penalty to non-edges
   Q.fill_diagonal(0)
-
-  penalty_reg = 2
   
+  return penalty_reg * Q, b
+
+def weighted_graph_to_qubo(G, penalty_reg=1):
+  """Transform a compatible weighted graph G into a QUBO matrix Q and bias b for 
+      the maximum clique problem.
+
+  Parameters
+  ----------
+  G : np.ndarray
+      Weighted edge strength matrix of the graph.
+      
+  Returns
+  -------
+  Q : numpy.ndarray
+      QUBO matrix.
+  b : numpy.ndarray
+      Bias vector.
+  """
+  n = G.shape[0]
+  Q = G
+  b = -np.ones(n)
+
   return penalty_reg * Q, b
 
 def solve_graph(G, solver):
