b521a5cc2d00b8d1186008608ba99cfb9978535b
xLynna <lynna.xie@gmail.com>
2023-06-11 01:49:40 +0100

    fix: formats of mask and length calculation

--- QUBO_formulation/limited_vertex.py
+++ QUBO_formulation/limited_vertex.py
@@ -49,9 +49,11 @@ def reduce_graph(G, lower_bound):
       Indices in terms of the input graph of the remain nodes in the subgraph.
   """
   meta_indices = _extract_k_core(G, lower_bound)
+  if len(meta_indices) == 0:
+    return []
   v = np.random.choice(len(meta_indices), 1)[0]
   v_neighbours_mask = G[v, :] == 1
-  common_neighbours = np.logical_and(G, v_neighbours_mask).sum(axis=1, keepdims=True)
+  common_neighbours = np.logical_and(G, v_neighbours_mask).sum(axis=1, keepdims=True).reshape(-1)
   # Remove the edges between the nodes if their common neighbours 
   # are less than the lower bound - 2
   G[v, common_neighbours < (lower_bound - 2)] = 0
@@ -133,11 +135,12 @@ def split(G, vertex_limit, lower_bound, solver):
       Indices of the nodes in the maximum clique.
   """
   max_clique_indices = None
+  ind = 0
   # negate graph size, graph, remaining nodes, splitted nodes
-  subgraphs = [(-G.shape[0], G, np.arange[G.shape[0]] , set())]
+  subgraphs = [(-G.shape[0], ind, G, np.arange(G.shape[0]), set())]
 
   while len(subgraphs) > 0 and -subgraphs[0][0] > vertex_limit:
-    _, sg, sg_indices, sg_pending_indices = heapq.heappop(subgraphs)
+    _, _, sg, sg_indices, sg_pending_indices = heapq.heappop(subgraphs)
     v_ind = _highest_degree_node(sg)
     ssg, ssg_raw_indeices = _extract_subgraph(sg, v_ind)
     # indices in terms of the input graph
@@ -151,26 +154,29 @@ def split(G, vertex_limit, lower_bound, solver):
     if sg_indices.shape[0] > 0:
       if sg_indices.shape[0] <= vertex_limit:
         # Obtain the max clique size and the related binary solution of the subgraph
-        sg_solution_mask = solve_graph(sg, solver)
+        sg_solution_mask = solve_graph(sg, solver).astype(bool)
         # The size should include the vertices removed due to vertex limit
-        combined_clique_size = len(sg_solution_mask) + len(sg_pending_indices)
+        combined_clique_size = len(np.argwhere(sg_solution_mask).flatten()) + len(sg_pending_indices)
         if combined_clique_size > lower_bound:
           lower_bound = combined_clique_size
-          max_clique_indices = set(sg_indices[sg_solution_mask]) + sg_pending_indices
+          max_clique_indices = set(sg_indices[sg_solution_mask]).union(sg_pending_indices)
+
       else:
-        heapq.heappush(subgraphs, (-sg.shape[0], sg, sg_indices, sg_pending_indices))
+        ind += 1
+        heapq.heappush(subgraphs, (-sg.shape[0], ind, sg, sg_indices, sg_pending_indices))
     
     # Calculate the max clique from ssg
     ssg_indices = ssg_indices[reduce_graph(ssg, lower_bound)]
     if ssg_indices.shape[0] > 0:
       if ssg_indices.shape[0] <= vertex_limit:
-        ssg_solution_mask = solve_graph(ssg, solver)
-        combined_clique_size = len(ssg_solution_mask) + len(sg_pending_indices) + 1 # v_ind
+        ssg_solution_mask = solve_graph(ssg, solver).astype(bool)
+        combined_clique_size = len(np.argwhere(ssg_solution_mask).flatten()) + len(sg_pending_indices) + 1 # v_ind
         if combined_clique_size > lower_bound:
           lower_bound = combined_clique_size
-          max_clique_indices = set(ssg_indices[ssg_solution_mask]) + sg_pending_indices + {v_ind}
+          max_clique_indices = set(ssg_indices[ssg_solution_mask]).union(sg_pending_indices, {v_ind})
+
       else:
-        heapq.heappush(subgraphs, (-ssg.shape[0], ssg, ssg_indices, sg_pending_indices + {v_ind}))
+        ind += 1
+        heapq.heappush(subgraphs, (-ssg.shape[0], ind, ssg, ssg_indices, sg_pending_indices.union({v_ind})))
 
   return max_clique_indices, lower_bound
-    
